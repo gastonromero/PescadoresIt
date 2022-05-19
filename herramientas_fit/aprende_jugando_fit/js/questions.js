f57e392f876de061e7de8d7a23da7238a8e618b3
MicaelaCayo <micaelacayo.edu@gmail.com>
2022-05-19 16:35:35 -0300

    Actualizacion

---
+++ herramientas_fit/aprende_jugando_fit/js/questions.js
@@ -0,0 +1,79 @@
+let MCQS = [{
+    question: "¿Qué significa HTML?",
+    choice1: "Hipervínculos y lenguaje de marcado de texto",
+    choice2: "Lenguaje de marcado de hipertexto",
+    choice3: "Lenguaje de creación de hipertexto",
+    choice4: "Lenguaje de marcas de hipertexto",
+    answer: 1
+},
+            {
+            question: "¿Qué significa CSS?",
+    choice1: "Hoja de estilo de colores",
+    choice2: "Hoja de estilo creativo",
+    choice3: "Hoja de estilo en cascada",
+    choice4: "Hoja de estilo del ordenador",
+    answer: 2
+},
+            {
+            question: "¿Qué etiqueta HTML se utiliza para definir una hoja de estilo interna?",
+    choice1: "<script>",
+    choice2: "<style>",
+    choice3: "<html>",
+    choice4: "<svg>",
+    answer: 1
+},
+            {
+            question: "¿Cuál es la sintaxis CSS correcta?",
+    choice1: "body{color:black}",
+    choice2: "{body{color:black}",
+    choice3: "body={color:black}",
+    choice4: "body:color{black}",
+    answer: 0
+},
+            {
+            question: "¿Cómo se inserta un comentario en un archivo CSS?",
+    choice1: "/*Esto es un comentario*/",
+    choice2: "//Esto es un comentario",
+    choice3: "<!---Esto es un comentario --->",
+    choice4: "//Esto es un comentario//",
+    answer: 1
+},
+            {
+            question: "¿En qué parte de la página se definen las hojas de estilos externas?",
+    choice1: "En el HEAD de la página",
+    choice2: "Justo después del elemento BODY",
+    choice3: "En cualquier parte de la página",
+    choice4: "Al final de la página",
+    answer: 0
+},
+            {
+               question: "¿Qué propiedad se utiliza para cambiar el color de fondo?",
+    choice1: "backgroundColor",
+    choice2: "BgColor",
+    choice3: "Color-Background",
+    choice4: "background",
+    answer: 3
+},{
+               question: "¿Cómo se escribe una sentencia IF en JavaScript?",
+    choice1: "if i==5",
+    choice2: "if(i==5)",
+    choice3: "if(i==5)then",
+    choice4: "if i==5 then",
+    answer: 2
+},
+            {
+               question: "¿Dentro de qué elemento HTML ponemos el JavaScript?",
+    choice1: "<js></js>",
+    choice2: "<javascript></javascript>",
+    choice3: "<script></script>",
+    choice4: "<scripting>",
+    answer: 2
+},
+            {
+               question: "¿Cómo empieza un bucle WHILE?",
+    choice1: "while(i <= 0)",
+    choice2: "while(i <= 0 i++)",
+    choice3: "while i <= 0",
+    choice4: "while (i++ i <= 0)",
+    answer: 0
+}];
